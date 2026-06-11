5a8f91112aee7098103b7f6d583a78676b93a265
SAPP-ThinkToInnovate <158750774+SAPP-ThinkToInnovate@users.noreply.github.com>
2026-06-11 16:36:53 +0530

    Create LoginForm.jsx

---
+++ UI Templates/react/Sample/LoginForm.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export default function LoginForm() {
+
+    return <form>
+        <label htmlFor="name">Name</label>
+        <input id="name" type="text" />
+    </form>
+}
